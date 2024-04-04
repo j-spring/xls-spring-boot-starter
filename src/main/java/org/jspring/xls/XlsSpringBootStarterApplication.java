855870123599245755b2b4dc8c14e4bc6d0333bb
daniele.gubbiotti <gubbiottidaniele@gmail.com>
2024-04-04 11:37:01 +0200

    fixing moving directory

---
+++ src/main/java/org/jspring/xls/XlsSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package org.jspring.xls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XlsSpringBootStarterApplication.class, args);
+    }
+
+}
